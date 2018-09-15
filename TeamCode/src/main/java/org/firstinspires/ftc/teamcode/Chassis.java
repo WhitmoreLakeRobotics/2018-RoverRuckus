8d0f7df46c65a8f45d71f05465d805fa2f6392cd
Bart Simpson <BartSimpson@gmail.com>
2018-09-15 15:59:26 -0400

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Chassis.java
@@ -0,0 +1,210 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.util.RobotLog;
+
+/**
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop_test for a two wheeled robot
+ * It includes all the skeletal structure that all iterative OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+//@TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
+//@Disabled
+public class Chassis extends OpMode
+{
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private static final String TAGChassis = "8492-Chassis";
+
+
+    public static final int ChassisMode_Stop = 0;
+    public static final int ChassisMode_Drive = 1;
+    public static final int ChassisMode_Turn = 2;
+    public static final int ChassisMode_Idle = 3;
+    public static final int ChassisMode_Teleop = 4;
+
+    //current mode of operation for Chassis
+    private int ChassisMode_Current = ChassisMode_Stop;
+
+
+    //LDM=leftDriveMotor
+    //RDM=rightDriveMotor
+    private  DcMotor LDM1 = null;
+    private  DcMotor LDM2 = null;
+    private  DcMotor RDM1 = null;
+    private  DcMotor RDM2 = null;
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+
+
+        RDM1 = hardwareMap.dcMotor.get("RDM1") ;
+        LDM1 = hardwareMap.dcMotor.get("LDM1") ;
+        LDM2 = hardwareMap.dcMotor.get("LDM2") ;
+        RDM2 = hardwareMap.dcMotor.get("RDM2") ;
+
+        if (LDM1 == null)  {
+            telemetry.log().add("LDM1 is null...");
+        }
+        if (LDM2 == null)  {
+            telemetry.log().add("LDM2 is null...");
+        }
+        if (RDM1 == null)  {
+            telemetry.log().add("RDM1 is null...");
+        }
+        if (RDM2 == null)  {
+            telemetry.log().add("RDM2 is null...");
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+
+    }
+    private void setMotorMode(DcMotor.RunMode newMode) {
+
+    LDM1.setMode (newMode);
+    RDM1.setMode (newMode);
+    LDM2.setMode (newMode);
+    RDM2.setMode (newMode);
+    }
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop(
+
+
+
+    ) {
+        // Setup a variable for each drive wheel to save power level for telemetry
+
+
+
+        // Choose to drive using either Tank Mode, or POV Mode
+        // Comment out the method that's not used.  The default below is POV.
+
+        // POV Mode uses left stick to go forward, and right stick to turn.
+        // - This uses basic math to combine motions and is easier to drive straight.
+
+
+
+
+        // Tank Mode uses one stick to control each wheel.
+        // - This requires no math, but it is hard to drive forward slowly and keep straight.
+        // leftPower  = -gamepad1.left_stick_y ;
+        // rightPower = -gamepad1.right_stick_y ;
+
+        // Send calculated power to wheels
+
+        // Show the elapsed game time and wheel power.
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("Motors", "left (%.2f), right (%.2f)");
+
+//        RobotLog.aa(TAGChassis,"Stage: "+ CurrentStage );
+        RobotLog.aa(TAGChassis, "Runtime: " + runtime.seconds());
+
+
+
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
+
+
+
+    public void doTeleop(double LDMpower,double RDMpower) {
+
+        RobotLog.aa(TAGChassis, "doTeleop: " + runtime.seconds());
+
+    LDM1.setPower(LDMpower);
+    RDM1.setPower(RDMpower);
+    LDM2.setPower(LDMpower);
+    RDM2.setPower(RDMpower);
+
+
+    }
+}
